6a81df65f64cc1f3e50ccf7f653f67bb9da713ee
scharanjit <charanjit.singh@imaginea.com>
2016-04-13 09:26:36 +0530

    vc

--- src/com/imaginea/test/TowersOfHanoi.scala
+++ src/com/imaginea/test/TowersOfHanoi.scala
@@ -2,10 +2,12 @@ package com.imaginea.test
 
 object TowersOfHanoi {
   
+  case class NegativeNotAllowed(message:String) extends Exception(message)
 def main(args: Array[String]): Unit = {
     println("Enter the number of disks")
     try{
     val v=scala.io.StdIn.readLine().toInt;
+    if (v<= 0) throw NegativeNotAllowed("Zero or Negative Numbers not allowed")
     towers(v, "Source", "Destination", "Pivot")
     }catch{
       case e: Exception => println("Invalid data !! exception caught: " + e);
@@ -18,7 +20,7 @@ def main(args: Array[String]): Unit = {
  
   def towers(x: Int, a: String, b: String, c: String): Unit = {
     if (x == 1) {
-      println("Single Disk moved from " + a + " to " + b)
+      println(" Disk" + x +" moved from " + a + " to " + b)
     } else {
       towers(x - 1, a, c, b)
       towers(1, a, b, c)
