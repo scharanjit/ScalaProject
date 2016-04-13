4ee68d8d5fd92b9af59698845e19060c711261cc
scharanjit <charanjit.singh@imaginea.com>
2016-04-13 14:21:20 +0530

    helloFinal

--- src/com/imaginea/test/StackTowersOFHanoi.scala
+++ src/com/imaginea/test/StackTowersOFHanoi.scala
@@ -40,25 +40,25 @@ object StackTowersOFHanoi {
       //first move to source to dest & reverse
       //& den source to aux & reverse
       if (k == 1) {
-        status = checkMove(sourceStack, destinationStack),x
+        status = checkMove(sourceStack, destinationStack)
         
-//        val arr= Array(checkMove(sourceStack, destinationStack),x)
+
         if ( status == 1) {
          stepNum=stepNum+1
          
-          println("Disk "+v+" moved from Source Tower to Destination Tower ")
+          println("Disk moved from Source Tower to Destination Tower ")
         } else if(status ==2){
          stepNum=stepNum+1
-          println("Disk "+v+"  moved from Destination Tower to Source Tower ")
+          println("Disk moved from Destination Tower to Source Tower ")
         }
         
         status = checkMove(sourceStack, pivotStack)
         if ( status == 1) {
      stepNum=stepNum+1
-          println("Disk "+v+" moved from Source Tower to Pivot Tower ")
+          println("Disk  moved from Source Tower to Pivot Tower ")
         } else if(status ==2){
          stepNum=stepNum+1
-          println("Disk "+v+" moved from Pivot Tower to Source Tower ")
+          println("Disk  moved from Pivot Tower to Source Tower ")
         }else{
          return
         }
@@ -69,7 +69,7 @@ object StackTowersOFHanoi {
         status = checkMove(sourceStack, pivotStack)
         if ( status == 1) {
           stepNum=stepNum+1
-          println("Disk "+v+" moved from Source Tower to Pivot Tower ")
+          println("Disk  moved from Source Tower to Pivot Tower ")
         } else if(status ==2){
            stepNum=stepNum+1
           println("Disk moved from Pivot Tower to Source Tower ")
@@ -79,10 +79,10 @@ object StackTowersOFHanoi {
         status = checkMove(sourceStack, destinationStack)
         if ( status == 1) {
           stepNum=stepNum+1
-          println("Disk "+v+" moved from Source Tower to Destination Tower ")
+          println("Disk moved from Source Tower to Destination Tower ")
         } else if(status ==2){
           stepNum=stepNum+1
-          println("Disk "+v+" moved from Destination Tower to Source Tower ")
+          println("Disk moved from Destination Tower to Source Tower ")
         }
 
       }
@@ -91,10 +91,10 @@ object StackTowersOFHanoi {
       status = checkMove(pivotStack, destinationStack)
         if ( status == 1) {
            stepNum=stepNum+1
-          println("Disk "+stepNum+" moved from Pivot Tower to Destination Tower ")
+          println("Disk  moved from Pivot Tower to Destination Tower ")
         } else if(status == 2){
            stepNum=stepNum+1
-          println("Disk "+stepNum+" moved from Destination Tower to Pivot Tower ")
+          println("Disk  moved from Destination Tower to Pivot Tower ")
         }
 
     } while (destinationStack.size != v)
@@ -108,7 +108,7 @@ object StackTowersOFHanoi {
    * 2.) checking weight or disk size
    * */
 
-  def checkMove(srcStack: Stack[Int], destStack: Stack[Int]): (Int,Int) = {
+  def checkMove(srcStack: Stack[Int], destStack: Stack[Int]): Int = {
 
     var i, j = 0
 
@@ -127,27 +127,27 @@ object StackTowersOFHanoi {
 //        println("Empty Stack")
         j = 0 //stack is empty
     }
-    if (i == j) return (0,i.toInt) //both towers are empty
+    if (i == j) return 0 //both towers are empty
 
     // if either of tower src and dest are empty
 
     if (i == 0) { //if tower A is empty ,pop from B push disk onto A
       srcStack.push(destStack.pop())
-      return (2,i.toInt)
+      return 2
     } else if (j == 0) { //if tower B is empty, pop from A push disk onto B
       destStack.push(srcStack.pop())
-      return (1,j.toInt)
+      return 1
     }
 
     //checking weight & move
     if (i < j) {
       destStack.push(srcStack.pop()) // move occurs from tower A to B i.e. source to destination
-      return (1,i.toInt)
+      return 1
     } else if (i > j) {
       srcStack.push(destStack.pop()) //move occurs from tower B to A i.e. destination to source
-      return (2,j.toInt)  
+      return 2  
     }
-    return (-1,-1)
+    return -1
 
   }
 }
