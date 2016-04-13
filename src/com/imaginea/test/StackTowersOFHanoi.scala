9b65ef447ab9307047482f0d3bbe02e0c8e9a22a
scharanjit <charanjit.singh@imaginea.com>
2016-04-13 13:50:07 +0530

    hello

--- src/com/imaginea/test/StackTowersOFHanoi.scala
+++ src/com/imaginea/test/StackTowersOFHanoi.scala
@@ -1,40 +1,153 @@
 package com.imaginea.test
 import scala.collection.mutable.Stack
+import java.util.NoSuchElementException
+import util.control.Breaks._
 
-case class NegativeNotAllowed(message:String) extends Exception(message)
+case class NegativeNotAllowed(message: String) extends Exception(message)
 
 object StackTowersOFHanoi {
   def main(args: Array[String]): Unit = {
     println("In Main")
-    var stepNum=0
-    var status=0
-    var v=0;
-    val sourceStack:Stack[Int]=new Stack
-    val pivotStack:Stack[Int]=new Stack
-    val destinationStack:Stack[Int]=new Stack
+    var stepNum = 0
     
-     println("Enter the number of disks")
-    try{
-     v=scala.io.StdIn.readLine().toInt;
-    if (v<= 0) throw NegativeNotAllowed("Zero or Negative Numbers not allowed")
+    var status = 0
     
-    }catch{
+    var v = 0;
+    val sourceStack: Stack[Int] = new Stack
+    val pivotStack: Stack[Int] = new Stack
+    val destinationStack: Stack[Int] = new Stack
+
+    println("Enter the number of disks")
+    try {
+      v = scala.io.StdIn.readLine().toInt;
+      if (v <= 0) throw NegativeNotAllowed("Zero or Negative Numbers not allowed")
+
+    } catch {
       case e: Exception => println("Invalid data !! exception caught: " + e);
     }
-   
-    
-   var i=0
-   
-   for (i <- 1 to v)
-     sourceStack.push(i)
-     
+
+    var i = 0
+
+    for (i <- (1 to v).reverse) {
+      sourceStack.push(i)
+    }
+
+    var k = v % 2
+    var x=0;
+    println(sourceStack+"--- "+k)
+    do {
+      //if odd disk 
+      //first move to source to dest & reverse
+      //& den source to aux & reverse
+      if (k == 1) {
+        status = checkMove(sourceStack, destinationStack),x
+        
+//        val arr= Array(checkMove(sourceStack, destinationStack),x)
+        if ( status == 1) {
+         stepNum=stepNum+1
+         
+          println("Disk "+v+" moved from Source Tower to Destination Tower ")
+        } else if(status ==2){
+         stepNum=stepNum+1
+          println("Disk "+v+"  moved from Destination Tower to Source Tower ")
+        }
+        
+        status = checkMove(sourceStack, pivotStack)
+        if ( status == 1) {
+     stepNum=stepNum+1
+          println("Disk "+v+" moved from Source Tower to Pivot Tower ")
+        } else if(status ==2){
+         stepNum=stepNum+1
+          println("Disk "+v+" moved from Pivot Tower to Source Tower ")
+        }else{
+         return
+        }
+      } else {
+        //if even
+        //source to pivot & reverse
+        //source to destination & reverse
+        status = checkMove(sourceStack, pivotStack)
+        if ( status == 1) {
+          stepNum=stepNum+1
+          println("Disk "+v+" moved from Source Tower to Pivot Tower ")
+        } else if(status ==2){
+           stepNum=stepNum+1
+          println("Disk moved from Pivot Tower to Source Tower ")
+        }
+        
+      
+        status = checkMove(sourceStack, destinationStack)
+        if ( status == 1) {
+          stepNum=stepNum+1
+          println("Disk "+v+" moved from Source Tower to Destination Tower ")
+        } else if(status ==2){
+          stepNum=stepNum+1
+          println("Disk "+v+" moved from Destination Tower to Source Tower ")
+        }
+
+      }
+      
+      
+      status = checkMove(pivotStack, destinationStack)
+        if ( status == 1) {
+           stepNum=stepNum+1
+          println("Disk "+stepNum+" moved from Pivot Tower to Destination Tower ")
+        } else if(status == 2){
+           stepNum=stepNum+1
+          println("Disk "+stepNum+" moved from Destination Tower to Pivot Tower ")
+        }
+
+    } while (destinationStack.size != v)
+
   }
-  
-  
-  
-  
-  def checkMove(A1 : Stack[Int],B1 :Stack[Int]):Any={
-    
-    
+
+  /*
+   * This method will move one disk from One tower to another
+   * depending upon different scenarios
+   * 1.) check emptiness
+   * 2.) checking weight or disk size
+   * */
+
+  def checkMove(srcStack: Stack[Int], destStack: Stack[Int]): (Int,Int) = {
+
+    var i, j = 0
+
+    //scenario 1  : checking when both towers are empty
+    try {
+      i = srcStack.top.toInt
+    } catch {
+      case e: Exception =>
+//        println("Empty Stack")
+        i = 0 //stack is empty
+    }
+    try {
+      j = destStack.top.toInt
+    } catch {
+      case e: Exception =>
+//        println("Empty Stack")
+        j = 0 //stack is empty
+    }
+    if (i == j) return (0,i.toInt) //both towers are empty
+
+    // if either of tower src and dest are empty
+
+    if (i == 0) { //if tower A is empty ,pop from B push disk onto A
+      srcStack.push(destStack.pop())
+      return (2,i.toInt)
+    } else if (j == 0) { //if tower B is empty, pop from A push disk onto B
+      destStack.push(srcStack.pop())
+      return (1,j.toInt)
+    }
+
+    //checking weight & move
+    if (i < j) {
+      destStack.push(srcStack.pop()) // move occurs from tower A to B i.e. source to destination
+      return (1,i.toInt)
+    } else if (i > j) {
+      srcStack.push(destStack.pop()) //move occurs from tower B to A i.e. destination to source
+      return (2,j.toInt)  
+    }
+    return (-1,-1)
+
   }
 }
