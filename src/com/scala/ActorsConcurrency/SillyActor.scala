f3b67370a6894723e37f314b24b96b051c25c05f
scharanjit <charanjit.singh@imaginea.com>
2016-05-02 14:49:57 +0530

    02052010.3

---
+++ src/com/scala/ActorsConcurrency/SillyActor.scala
@@ -0,0 +1,12 @@
+package com.scala.ActorsConcurrency
+
+import scala.actors._
+
+object SillyActor extends Actor {
+  def act() {
+    for (i <- 1 to 5) {
+      println("I'm acting!")
+      Thread.sleep(1000)
+    }
+  }
+}
