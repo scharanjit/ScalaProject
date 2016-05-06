720155312a835e353d969336c9b8c907c66971f3
scharanjit <charanjit.singh@imaginea.com>
2016-05-06 15:23:07 +0530

    06052016

---
+++ src/com/scala/ObjectClass/ChecksumAccumulator.scala
@@ -0,0 +1,13 @@
+package com.scala.ObjectClass
+
+class ChecksumAccumulator {
+   var sum=0
+   
+     def add(b: Byte): Unit = {
+      sum += b
+    }
+  
+    def checksum(): Int = {
+      return ~(sum & 0xFF) + 1
+    }
+}
---
+++ src/com/scala/ObjectClass/wscx.sc
@@ -0,0 +1,11 @@
+package com.scala.ObjectClass
+
+object wscx {
+  println("Welcome to the Scala worksheet")
+  
+   var ac= new ChecksumAccumulator
+  ac.sum=9
+  
+  
+  
+}
